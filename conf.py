c07a95ea8c2c4e66ba92f6f80b31e374168643db
Björn Bengtsson <bjorn.bengtsson@it.cdon.com>
2017-04-03 11:44:42 +0200

    Cleaned config.

--- conf.py
+++ conf.py
@@ -1,34 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-#
-# source_suffix = ['.rst', '.md']
-##source_suffix = '.rst'
-
 # The master toctree document.
 master_doc = 'index'
 
 # General information about the project.
 project = 'Marketplace Documentation'
 copyright = '2017, CDON AB'
-#author = 'CDON AB'
-
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
-# The short X.Y version.
-##version = '0.1'
-# The full version, including alpha/beta/rc tags.
-##release = '0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', '.git', '.gitignore']
+exclude_patterns = ['_build', '**/.git', '.gitignore']
 
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
